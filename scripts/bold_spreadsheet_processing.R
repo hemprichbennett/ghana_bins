763afeec41321e458e5079b96034b32f6d2183a5
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2021-12-10 12:09:44 +0000

    Added some comments and saved the figure

--- scripts/bold_spreadsheet_processing.R
+++ scripts/bold_spreadsheet_processing.R
@@ -1,3 +1,7 @@
+# Code for reading in and formatting basic data from BOLD
+# Setup -------------------------------------------------------------------
+
+# load packages
 library(readxl)
 library(purrr)
 library(dplyr)
@@ -5,6 +9,10 @@ library(janitor)
 library(tidyr)
 library(lubridate)
 library(ggplot2)
+
+
+# Read in the data
+
 # this assumes that the input data has had the redundant first two rows
 # (eyeroll) of the raw data on each spreadsheet removed
 xl_path <- ('data/processed_data/bold_tidied.xlsx')
@@ -21,9 +29,22 @@ bold_data <- xl_path %>%
   # make the dates usable, rather than a string
   mutate(collection_date = dmy(collection_date))
 
+
+
+# Basic plotting ----------------------------------------------------------
+
+
+
 # make a simple plot of the number of SAMPLES over time
-ggplot(bold_data, aes(x = collection_date)) + 
+collection_date_histogram <- ggplot(bold_data, aes(x = collection_date)) + 
   geom_histogram() +
   theme_bw() +
   xlab('Collection date') + 
   ylab('Number of samples sequenced by December 10th 2021')
+# show it
+collection_date_histogram
+
+# save it
+ggsave('figures/collection_date_histogram.jpeg', collection_date_histogram)
+
+
