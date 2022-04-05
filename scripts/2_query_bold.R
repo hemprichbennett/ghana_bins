789525a8c95895c33851a5bda18066f1b2f81845
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-04-05 17:00:11 +0100

    Fixed a fairly important typo...

--- scripts/2_query_bold.R
+++ scripts/2_query_bold.R
@@ -5,7 +5,8 @@ in_df <- read_csv('data/processed_data/tidied_bold_data.csv')
 
 unique_bins <- in_df %>%
   filter(!is.na(bin)) %>%
-  pull(bin)
+  pull(bin) %>%
+  unique(.)
 
 bold_sequence_list <- list()
 
