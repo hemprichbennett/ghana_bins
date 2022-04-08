c8f560db30872fd0ac48bb1b2a557d5007f1ee6c
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-04-08 16:20:55 +0100

    Added regional and country summary plots and tables

--- scripts/4_BIN_geography.R
+++ scripts/4_BIN_geography.R
@@ -80,10 +80,113 @@ public_bins_df %>%
   group_by(bin_uri, geographic_region, order_name) %>%
   summarise(nsamples = n()) %>%
   group_by(geographic_region, nsamples, order_name) %>%
-  summarise(nmatches = n()) #%>%
+  summarise(nmatches = n()) %>%
   ggplot(., aes(x = nsamples, y = nmatches))+
   geom_point()+ 
   facet_grid( geographic_region ~ order_name) +
   theme_bw()+
   scale_y_log10() +
   scale_x_log10() 
+
+# make an object thats just the number of BINs we have for each 
+# taxonomic order
+distinct_bins <- public_bins_df %>%
+  filter(!is.na(order_name), 
+         !is.na(geographic_region)) %>%
+  select(order_name, bin_uri) %>%
+  distinct() %>%
+  group_by(order_name) %>%
+  summarise(global_distinct_bins = n())
+  
+region_percentage_BINs_present <- public_bins_df %>%
+  filter(!is.na(order_name), 
+         !is.na(geographic_region)) %>%
+  select(order_name, bin_uri, geographic_region) %>%
+  distinct() %>%
+  group_by(order_name, geographic_region) %>%
+  summarise(local_n_bins = n()) %>%
+  ungroup() %>%
+  left_join(distinct_bins) %>%
+  mutate(
+    # calculate the percentage of all public BINs which we've
+    # collected that are known from a given geographic region
+    percentage = local_n_bins / global_distinct_bins * 100,
+    plotting_label = paste0(order_name, ', ', global_distinct_bins, ' public BINs')) 
+
+
+ggplot(region_percentage_BINs_present, 
+       aes(x = geographic_region,
+           y = fct_rev(plotting_label),
+           fill = percentage)) +
+  geom_tile() + 
+  theme_bw() +
+  theme(legend.position = 'bottom',
+        axis.text.x = element_text(angle = 45, hjust=1)) + 
+  labs(x = 'Geographic region', y = 'Taxonomic order')
+
+
+geographic_region_bin_summary <- region_percentage_BINs_present %>%
+  select(geographic_region, plotting_label, local_n_bins) %>%
+  pivot_wider(names_from = plotting_label, 
+              values_from = local_n_bins,
+              values_fill = 0) %>%
+  arrange(desc(geographic_region))
+write_csv(geographic_region_bin_summary, 'results/public_data/orders_by_region.csv')
+
+
+# Now for countries within Africa and the Middle East
+
+
+country_percentage_BINs_present <- public_bins_df %>%
+  filter(!is.na(order_name), 
+         !is.na(geographic_region)) %>%
+  # filter for regions which include the word 'Africa'
+  filter(grepl('Africa', geographic_region)) %>%
+  select(order_name, bin_uri, country) %>%
+  distinct() %>%
+  group_by(order_name, country) %>%
+  summarise(local_n_bins = n()) %>%
+  ungroup() %>%
+  left_join(distinct_bins) %>%
+  mutate(
+    # calculate the percentage of all public BINs which we've
+    # collected that are known from a given geographic region
+    percentage = local_n_bins / global_distinct_bins * 100,
+    plotting_label = paste0(order_name, ', ', global_distinct_bins, ' public BINs')) 
+
+
+ggplot(country_percentage_BINs_present, 
+       aes(x = country,
+           y = fct_rev(plotting_label),
+           fill = percentage)) +
+  geom_tile() + 
+  theme_bw() +
+  theme(legend.position = 'bottom',
+        axis.text.x = element_text(angle = 45, hjust=1)) + 
+  labs(x = 'Country', y = 'Taxonomic order')
+
+
+country_bin_summary <- country_percentage_BINs_present %>%
+  select(country, plotting_label, local_n_bins) %>%
+  pivot_wider(names_from = plotting_label, 
+              values_from = local_n_bins,
+              values_fill = 0) %>%
+  arrange(desc(country))
+write_csv(country_bin_summary, 'results/public_data/orders_by_country.csv')
+
+
+# lets make a heatmap of only countries that have over n bins in common with
+# us
+bin_threshold <- 10
+country_percentage_BINs_present %>% 
+  group_by(country) %>% 
+  summarise(n = sum(local_n_bins)) %>%
+  filter(n >= bin_threshold) %>%
+  left_join(country_percentage_BINs_present) %>%
+  select(country, plotting_label, local_n_bins) %>%
+  pivot_wider(names_from = plotting_label, 
+              values_from = local_n_bins,
+              values_fill = 0) %>%
+  arrange(desc(country)) %>%
+  write_csv('results/public_data/orders_in_commonest_countries.csv')
+
