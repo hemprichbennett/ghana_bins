c378004d10dc235d50406aecc18576ea145138ac
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-04-08 12:08:43 +0100

    Made some ugly chaotic ggplots, definitely WIP

--- scripts/4_BIN_geography.R
+++ scripts/4_BIN_geography.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(countrycode)
 
+
 our_samples_df <- read_csv('data/processed_data/bold_data_with_availability.csv')
 
 public_bins_df <- read_csv('data/processed_data/bold_public_bin_matches.csv') %>%
@@ -34,8 +35,55 @@ public_bins_df %>%
 
 public_bins_df %>%
   group_by(geographic_region) %>%
-  summarise(nsamples = n())
+  summarise(nsamples = n(),
+            nbins = length(unique(bin_uri)))
 # Matches that are NA for country or region are because they were mined from 
 # Genbank and have no geographic data attached
 
+public_bins_df %>%
+  group_by(bin_uri, geographic_region) %>%
+  summarise(nsamples = n()) %>%
+  ggplot(., aes(x = nsamples))+
+  geom_histogram() +
+  facet_wrap(. ~ geographic_region) + 
+  scale_y_log10() +
+  scale_x_log10() +
+  theme_bw() +
+  labs(x = 'Number of samples per BIN',
+       y = 'Number of BINs')
 
+# this plot is a bit confusing, but to summarise: the value on the left hand
+# side is the number of BINs which only had ONE public sample in a given geographic
+# region. As we progress to the right we have BINs which had tens, hundreds and thousands
+# of samples in that geographic region
+
+
+
+
+public_bins_df %>%
+  filter(!is.na(geographic_region)) %>%
+  group_by(bin_uri, geographic_region) %>%
+  summarise(nsamples = n()) %>%
+  group_by(geographic_region, nsamples) %>%
+  summarise(nmatches = n()) %>%
+  ggplot(., aes(x = nsamples, y = nmatches))+
+  geom_point()+ 
+  facet_wrap(. ~ geographic_region) +
+  theme_bw()+
+  scale_y_log10() +
+  scale_x_log10() 
+# I don't even really know what this is showing rn
+
+public_bins_df %>%
+  filter(geographic_region %in% c("Sub-Saharan Africa",
+                                  "Middle East & North Africa")) %>%
+  group_by(bin_uri, geographic_region, order_name) %>%
+  summarise(nsamples = n()) %>%
+  group_by(geographic_region, nsamples, order_name) %>%
+  summarise(nmatches = n()) #%>%
+  ggplot(., aes(x = nsamples, y = nmatches))+
+  geom_point()+ 
+  facet_grid( geographic_region ~ order_name) +
+  theme_bw()+
+  scale_y_log10() +
+  scale_x_log10() 
