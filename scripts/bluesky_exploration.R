5f93f917b9075df03a9e252ddd645c9771fbcc07
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-10-31 15:46:26 +0000

    Made temporary script for exploratory work

---
+++ scripts/bluesky_exploration.R
@@ -0,0 +1,8 @@
+# script has no defined purpose yet, I want to poke the
+# latest iteration of our data
+library(tidyverse)
+
+fortified_data <- read_csv('data/processed_data/fortified.csv')
+our_big_df <- read_csv('data/processed_data/our_organised_bold_data.csv')
+
+
