00cfe59b284821966e2689c555d562f9daf01fb0
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-10-31 16:27:36 +0000

    Started altering the plot to a nicer stacked barplot

--- scripts/bluesky_exploration.R
+++ scripts/bluesky_exploration.R
@@ -10,16 +10,14 @@ unique(our_big_df$sampling_protocol)
 
 # function to filter and reorganise our data, then plot
 # stacked barplots (I know...) of them
-stack_bar <- function(data, filter_by){
+tileplot <- function(data, filter_by){
   data %>%
-    select(bin, sampling_protocol, order, family, genus, exact_site) %>%
     filter(sampling_protocol == filter_by) %>%
+    select(bin, sampling_protocol, order, family, genus, exact_site) %>%
     group_by(order, exact_site) %>%
     summarise(n_bins = n()) %>%
-  ggplot(., aes(x = exact_site, y = n_bins, fill = order)) +
-    # position=fill makes the stack proportional
-    geom_bar(position="fill", stat="identity")+
-    scale_fill_viridis_d()+
+  ggplot(., aes(x = exact_site, y = order)) +
+    geom_tile(aes(fill = n_bins), colour = "grey50")+
     theme_classic() +
     theme(legend.position = 'bottom')+
     xlab('Site')+
@@ -27,8 +25,8 @@ stack_bar <- function(data, filter_by){
     ggtitle(paste0('Stacked barplot of the BINs collected by ', filter_by, 's'))
 }
 
-stack_bar(our_big_df, filter_by = 'Sweep Net')    
-stack_bar(our_big_df, filter_by = 'Heath Trap')  
+tileplot(our_big_df, filter_by = 'Sweep Net')    
+tileplot(our_big_df, filter_by = 'Heath Trap')  
   
 
 # for some reason there are no sweep net samples listed from Abutia
