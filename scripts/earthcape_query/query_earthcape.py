f3d57cf01a0937c966132a999e031531e3b44b23
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-11-22 11:55:26 +0000

    rearranged repo for tidiness

--- scripts/earthcape_query/query_earthcape.py
+++ scripts/earthcape_query/query_earthcape.py
@@ -11,6 +11,12 @@ conn = pymssql.connect(server=params.server, user=params.user,
 
 desired_queries=['Unit', 'DNAextract', 'LocalityVisit']
 
+# brief override to get all items
+cursor = conn.cursor()  
+desired_queries=[]
+cursor.execute("SELECT name FROM SYSOBJECTS WHERE xtype = 'U'")
+for row in cursor:
+    desired_queries.append(row[0])
 
 for field in desired_queries:
     print(field)
@@ -19,7 +25,7 @@ for field in desired_queries:
     # check to see if the df contains anything (has a size greater than zero)
     if df.size > 0:
         # write the file
-        df.to_csv('./data/earthcape_query/' + field + '.csv')
+        df.to_csv('data/earthcape_query/' + field + '.csv')
     else:
         print(field + ' has zero size')
  
