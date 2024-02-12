9901dfb961ef2133856a3a06b2473874ed5a46f1
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-02-12 12:10:58 +0000

    Changed tag of image used

--- container_files/rstudio_server.sh
+++ container_files/rstudio_server.sh
@@ -1,6 +1,6 @@
 docker run --rm -ti -e PASSWORD=password -p 8781:8787 \
   -v .:/home/rstudio/bin_accumulation \
   -v /Users/dhb/.config/rstudio/rstudio-prefs.json:/home/rstudio/.config/rstudio/rstudio-prefs.json \
-  hemprichbennett/bin_accumulation_img:2024-02-02
+  hemprichbennett/bin_accumulation_img:2024-02-05
 
     #hemprichbennett/op1_net_img:2024-01-29
