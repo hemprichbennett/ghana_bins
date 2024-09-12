a8632ed7ec643cae6cbf42bbf2ccb0aef1087a56
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-09-12 16:09:01 +0100

    Changed date of image to use

--- container_files/rstudio_server.sh
+++ container_files/rstudio_server.sh
@@ -1,6 +1,4 @@
 docker run --rm -ti -e PASSWORD=password -p 8781:8787 \
   -v .:/home/rstudio/bin_accumulation \
   -v /Users/dhb/.config/rstudio/rstudio-prefs.json:/home/rstudio/.config/rstudio/rstudio-prefs.json \
-  hemprichbennett/bin_accumulation_img:2024-02-12
-
-    #hemprichbennett/op1_net_img:2024-01-29
+  hemprichbennett/bin_accumulation_img:2024-09-12
