5c93899d4e7ddc92459758a246ec607c976857dd
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-04-16 11:27:52 +0100

    Added debugging, basic plot and summary table

--- scripts/8_malaise_trap_analyses.R
+++ scripts/8_malaise_trap_analyses.R
@@ -7,3 +7,31 @@ malaise_trap_deployments <- read_csv(here('data', 'processed_data',
 
 all_arthropod_data <- read_csv(here('data', 'processed_data', 
                                     'bold_and_earthcape_combined.csv'))
+
+# ids to check and perhaps modify the times of
+bad_ids <- c(1000, 1011, 1099, 567, 577, 586, 604, 835)
+bad_id_pattern <- paste(bad_ids, collapse = '\\.|^')
+bad_id_pattern <- paste0('^', bad_id_pattern)
+bad_id_pattern <- paste0(bad_id_pattern, '\\.')
+
+# get traps which match the bad id pattern
+traps_of_interest <- all_arthropod_data %>% 
+  filter(grepl(bad_id_pattern, lot)) %>%
+  select(class, order, family, genus, species, bin, lot)
+
+
+malaise_trap_sample_counts <- all_arthropod_data %>%
+  filter(type == 'Malaise') %>%
+  group_by(lot) %>%
+  summarise(nsamples_sequenced = n())
+
+
+malaise_trap_bottle_counts <- malaise_trap_sample_counts %>%
+  mutate(overall_lot = gsub('\\..+', '', lot)) %>%
+  group_by(overall_lot) %>%
+  summarise(n_bottles = n()) %>%
+  group_by(n_bottles) %>%
+  summarise(n = n())
+
+ggplot(malaise_trap_bottle_counts, aes(x = n_bottles)) +
+  geom_histogram()
