927d67ecfa2bbb03116e62393e2b7edd6ad4ed0e
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-07-10 10:47:39 +0100

    Added malaise trap temporal calcs, capitalised some variables for nicer output files

--- scripts/12_general_taxonomy.R
+++ scripts/12_general_taxonomy.R
@@ -11,7 +11,11 @@ library(here)
 # read in data
 big_in_tib <- read_csv(, 
          file = here('data', 'processed_data', 
-                     'bold_and_earthcape_combined.csv'))
+                     'bold_and_earthcape_combined.csv')) %>%
+  rename(Order = order,
+         Family = family,
+         Genus = genus,
+         Species = species)
 
 malaise_trap_metadata <- read_csv(file = here('data', 'processed_data',
                                               'malaise_trap_metadata.csv'))
@@ -22,38 +26,67 @@ malaise_trap_metadata <- read_csv(file = here('data', 'processed_data',
 
 
 species_trap_counts <- big_in_tib %>%
-  filter(!is.na(species), !is.na(type)) %>%
-  group_by(order, family, species, type) %>%
+  filter(!is.na(Species), !is.na(type)) %>%
+  group_by(Order, Family, Species, type) %>%
   summarise(n_samples = n()) %>%
   pivot_wider(names_from = type, values_from = n_samples, 
               values_fill = list(n_samples = 0)) %>%
-  # sort the rows by order, then family, then species
-  arrange(order, family, species) %>%
-  select(order, family, species, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
-  
+  # sort the rows by Order, then Family, then Species
+  arrange(Order, Family, Species) %>%
+  select(Order, Family, Species, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
+
+write_csv(species_trap_counts, 
+          file = here('results', 'taxonomic_summaries', 'species_trap_counts.csv'))  
 
 genus_trap_counts <- big_in_tib %>%
-  filter(!is.na(genus), !is.na(type)) %>%
-  group_by(order, family, genus, type) %>%
+  filter(!is.na(Genus), !is.na(type)) %>%
+  group_by(Order, Family, Genus, type) %>%
   summarise(n_samples = n()) %>%
   pivot_wider(names_from = type, values_from = n_samples, 
               values_fill = list(n_samples = 0)) %>%
-  arrange(order, family, genus) %>%
-  select(order, family, genus, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
+  arrange(Order, Family, Genus) %>%
+  select(Order, Family, Genus, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
+
+write_csv(genus_trap_counts, 
+          file = here('results', 'taxonomic_summaries', 'genus_trap_counts.csv'))  
 
 family_trap_counts <- big_in_tib %>%
-  filter(!is.na(family), !is.na(type)) %>%
-  group_by(order, family, type) %>%
+  filter(!is.na(Family), !is.na(type)) %>%
+  group_by(Order, Family, type) %>%
   summarise(n_samples = n()) %>%
   pivot_wider(names_from = type, values_from = n_samples, 
               values_fill = list(n_samples = 0)) %>%
-  arrange(order, family) %>%
-  select(order, family, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
+  arrange(Order, Family) %>%
+  select(Order, Family, Cdc, Heath, Malaise, Pitfall, `Yellow Pan`)
 
+write_csv(family_trap_counts, 
+          file = here('results', 'taxonomic_summaries', 'family_trap_counts.csv'))  
 
 # Make malaise trap temporal abundance table -----------------------------------
 
+# some malaise trap lots have no associated temporal data. Make a 
+# summary of them
+missing_temporal_information <- big_in_tib %>%
+  filter(type == 'Malaise', !is.na(Family)) %>%
+  left_join(malaise_trap_metadata) %>%
+  relocate(lot, coarse_timing) %>%
+  filter(is.na(coarse_timing))
 
+unique(missing_temporal_information$lot)
+
+
+# summarise the temporal data that we can
+malaise_trap_arthropods <- big_in_tib %>%
+  filter(type == 'Malaise', !is.na(Family)) %>%
+  left_join(malaise_trap_metadata) %>%
+  filter(!is.na(coarse_timing)) %>%
+  group_by(Order, Family, coarse_timing) %>%
+  summarise(n_samples = n()) %>%
+  pivot_wider(names_from = coarse_timing, values_from = n_samples, 
+              values_fill = list(n_samples = 0)) %>%
+  arrange(Order, Family) %>%
+  select(Order, Family, Day, Night)
+  
 
 # Pest species ------------------------------------------------------------
 
@@ -63,10 +96,10 @@ defra_pests <- read_csv(here('data', 'raw_data', 'Risk Register 09_July_2025 16_
   janitor::clean_names()
 
 # combine it with our species occurrences
-pest_detections <- inner_join(species_trap_counts, defra_pests, by = c('species' = 'pest_name'))
+pest_detections <- inner_join(species_trap_counts, defra_pests, by = c('Species' = 'pest_name'))
 
 
 # write file
 write_csv(pest_detections, 
-          file = here('results', 'pest_detections.csv'))
+          file = here('results', 'taxonomic_summaries', 'pest_detections.csv'))
 
