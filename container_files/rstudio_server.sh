a53b9d01400ff8b479fcb0adc4fde88818ac2647
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-07-10 15:42:13 +0100

    Added geosphere package to image

--- container_files/rstudio_server.sh
+++ container_files/rstudio_server.sh
@@ -1,5 +1,5 @@
 docker run --rm -ti -e PASSWORD=password -p 8781:8787 \
   -v .:/home/rstudio/bin_accumulation \
   -v /Users/dhb/.config/rstudio/rstudio-prefs.json:/home/rstudio/.config/rstudio/rstudio-prefs.json \
-  hemprichbennett/bin_accumulation_img:2025-06-10
+  hemprichbennett/bin_accumulation_img:2025-07-10
 
