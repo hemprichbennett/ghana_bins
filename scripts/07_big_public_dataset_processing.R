0341408c10e1e287e2ea4f5416b3d4f60e3d5d1f
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-07-30 11:37:29 +0100

    Changed path to big dataset

--- scripts/07_big_public_dataset_processing.R
+++ scripts/07_big_public_dataset_processing.R
@@ -6,7 +6,7 @@ library(vroom)
 # read in the huge (23GB) file, specifying that we solely want the columns for
 # the bin and country. File downloaded from
 # https://v4.boldsystems.org/index.php/datapackages
-all_public_bins <- vroom('/home/rstudio/bin_accumulation/data/raw_data/BOLD_Public.11-Jul-2025.tsv', delim ='\t',
+all_public_bins <- vroom(here('~/data/manual_bold_queries/BOLD_Public.11-Jul-2025/BOLD_Public.11-Jul-2025.tsv'), delim ='\t',
                          col_select = c('bin_uri', 'country/ocean')) %>%
   rename(country = `country/ocean`) %>%
   # calculate some basic stats for use in plotting in script 8
