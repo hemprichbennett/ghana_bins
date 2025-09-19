cce73e8e6ca4160e9869b4c7328f72ecff0fe26a
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-09-19 10:44:37 +0100

    Fixed bold package install error in Dockerfile and rebuilt image

--- container_files/rstudio_server_no_bold_file.sh
+++ container_files/rstudio_server_no_bold_file.sh
@@ -1,6 +1,6 @@
 docker run --rm -ti -e PASSWORD=password -p 8781:8787 \
   -v .:/home/rstudio/bin_accumulation \
   -v /Users/dhb/.config/rstudio/rstudio-prefs.json:/home/rstudio/.config/rstudio/rstudio-prefs.json \
-  hemprichbennett/bin_accumulation_img:2025-07-11
+  hemprichbennett/bin_accumulation_img:2025-09-19
 
 
